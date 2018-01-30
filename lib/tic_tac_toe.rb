c4e7bb65d8ab3545905bf3444e8dc3a9a280dc6d
William Falcone <wjf0003+will@gmail.com>
2018-01-30 22:06:38 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -92,4 +92,16 @@ class TicTacToe
     end
   end
   
+  def play(board)
+    while !over?(board)
+      turn(board)
+    end
+    if won?(board)
+     puts "Congratulations #{winner(board)}!"
+    elsif draw?(board)
+      puts "Cat's Game!"
+    end
+end
+  
+  
 end 
