088ef2fe7d888ef6e4f4b672acfe7c17d2408da8
William Falcone <wjf0003+will@gmail.com>
2018-01-30 21:42:21 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -36,6 +36,6 @@ class TicTacToe
   end
   
   def valid_move?(index)
-  index.between?(0,8) && !position_taken?(@board,index)
-end
+    index.between?(0,8) && !position_taken?(@board,index)
+  end
 end 
