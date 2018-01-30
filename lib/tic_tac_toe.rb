89682681355dd26609f09014cb7c13b134e0315c
William Falcone <wjf0003+will@gmail.com>
2018-01-30 21:58:52 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -65,11 +65,11 @@ class TicTacToe
     turn_count % 2 == 0 ? "X" : "O"
   end
   
-  def won?(board) 
+  def won? 
   WIN_COMBINATIONS.detect do |combo|
-      board[combo[0]] == board[combo[1]] &&
-      board[combo[1]] == board[combo[2]] &&
-      position_taken?(board, combo[0])
+      @board[combo[0]] == @board[combo[1]] &&
+      @board[combo[1]] == @board[combo[2]] &&
+      position_taken?(combo[0])
     end
   end
 end 
