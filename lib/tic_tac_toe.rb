c496abe3bf6e35fdb7124465c8cfb3c5853528cd
William Falcone <wjf0003+will@gmail.com>
2018-01-30 21:40:36 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -31,7 +31,7 @@ class TicTacToe
     @board[input] = player
   end
   
-  def position_taken?(@board,input)
-    @board[index]== "X" || board[index] == "O"
+  def position_taken?(@board,index)
+    @board[index]== "X" || @board[index] == "O"
   end
 end 
